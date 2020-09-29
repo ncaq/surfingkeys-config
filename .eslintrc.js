b2bd2f422d719063911689f705d41097baa8ce89
ncaq <ncaq@ncaq.net>
2020-09-29 20:02:32 +0900

    updated: eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,24 @@
-{
-  "env": {
-    "browser": true,
-    "webextensions": true
+module.exports = {
+  env: {
+    browser: true,
+    webextensions: true,
   },
-  "extends": [
-    "airbnb-base",
-    "plugin:prettier/recommended",
-    "prettier",
-    "prettier/@typescript-eslint"
-  ],
-  "plugins": ["mysticatea"],
-  "rules": {
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["mysticatea"],
+  rules: {
     "mysticatea/no-instanceof-array": "error",
     "mysticatea/no-instanceof-wrapper": "error",
     "mysticatea/no-literal-call": "error",
     "mysticatea/no-this-in-static": "error",
     "mysticatea/no-use-ignored-vars": "error",
     "mysticatea/no-useless-rest-spread": "error",
-    "curly": ["error", "all"],
+    curly: ["error", "all"],
     "prefer-destructuring": [
       "error",
       {
-        "array": false,
-        "object": true
-      }
-    ]
-  }
-}
+        array: false,
+        object: true,
+      },
+    ],
+  },
+};
