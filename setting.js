40bb06943a652c002679902a9eb2eb6da7e805bd
ncaq <ncaq@ncaq.net>
2021-12-13 10:10:08 +0900

    updated: エゴサーチ用のクエリをユーザ名回避にする

--- setting.js
+++ setting.js
@@ -282,7 +282,7 @@ mapkey("<Alt-;>", "通知 / Twitter", () => {
 
 mapkey("<Ctrl-Alt-;>", "エゴサーチ / Twitter", () => {
   tabOpenLink(
-    "https://twitter.com/search?q=%22ncaq%22%20OR%20%22%E3%82%A8%E3%83%8C%E3%83%A6%E3%83%AB%22%20OR%20%22%E3%81%88%E3%81%AC%E3%82%86%E3%82%8B%22%20OR%20twitter.com%2Fncaq%20-from%3Ancaq%20lang%3Aen%20OR%20lang%3Aja&f=live"
+    "https://twitter.com/search?q=%22ncaq%22%20OR%20%22%E3%82%A8%E3%83%8C%E3%83%A6%E3%83%AB%22%20OR%20%22%E3%81%88%E3%81%AC%E3%82%86%E3%82%8B%22%20OR%20twitter.com%2Fncaq%20-from%3Ancaq%20OR%20%40ncaq_do_not_exist&f=live"
   );
 });
 
