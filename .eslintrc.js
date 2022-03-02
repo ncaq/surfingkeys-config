83e6d386deea0eb43bf182401b0590f94b82f8e5
ncaq <ncaq@ncaq.net>
2022-03-02 19:25:42 +0900

    deleted: eslint-plugin-mysticatea

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,7 @@ module.exports = {
     webextensions: true,
   },
   extends: ["airbnb-base", "prettier"],
-  plugins: ["mysticatea"],
   rules: {
-    "mysticatea/no-instanceof-array": "error",
-    "mysticatea/no-instanceof-wrapper": "error",
-    "mysticatea/no-literal-call": "error",
-    "mysticatea/no-this-in-static": "error",
-    "mysticatea/no-use-ignored-vars": "error",
-    "mysticatea/no-useless-rest-spread": "error",
     curly: ["error", "all"],
     "prefer-destructuring": [
       "error",
